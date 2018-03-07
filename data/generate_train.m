d03aab6f8f7ea0ade39a9469618c3ca3b12659e6
Jiu XU <twtygqyy@gmail.com>
2018-03-07 11:01:38 -0500

    fix issue, change to right script

--- data/generate_train.m
+++ data/generate_train.m
@@ -11,13 +11,13 @@ size_label = 128;
 size_input = size_label/scale;
 stride = 128;
 
-data = zeros(size_input, size_input, 3, 1);
-label = zeros(size_label, size_label, 3, 1);
+data = zeros(size_input/4, size_input/4, 1, 1);
+label = zeros(size_label, size_label, 1, 1);
 
 count = 0;
 margain = 0;
 
-%% generate data
+%% generate data change image format to according to your image type
 filepaths = [];
 filepaths = [filepaths; dir(fullfile(folder, '*.JPEG'))];
 
@@ -26,30 +26,28 @@ length(filepaths)
 for i = 1 : length(filepaths)
     image = imread(fullfile(folder,filepaths(i).name));
     if size(image,3)==3
+        image = rgb2ycbcr(image);
         image = im2double(image);
         im_label = modcrop(image, scale);
-        [hei,wid, c] = size(im_label);
+        [hei,wid] = size(im_label);
 
         filepaths(i).name
         for x = 1 + margain : stride : hei-size_label+1 - margain
             for y = 1 + margain :stride : wid-size_label+1 - margain
-                subim_label = im_label(x : x+size_label-1, y : y+size_label-1, :);
-                subim_input = imresize(subim_label,1/scale,'bicubic');
-                % figure;
-                % imshow(subim_input);
-                % figure;
-                % imshow(subim_label);
+                subim_label = im_label(x : x+size_input-1, y : y+size_input-1);
+                subim_input = imresize(subim_label,1/scale(s),'bicubic');
+
                 count=count+1;
-                data(:, :, :, count) = subim_input;
-                label(:, :, :, count) = subim_label;
+                data(:, :, 1, count) = subim_input;
+                label(:, :, 1, count) = subim_label;
             end
         end
     end
 end
 
 order = randperm(count);
-data = data(:, :, :, order);
-label = label(:, :, :, order);
+data = data(:, :, 1, order);
+label = label(:, :, 1, order);
 
 %% writing to HDF5
 chunksz = 64;
